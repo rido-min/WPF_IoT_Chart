14ddeb907b2bacf5d18bb65b9f5a9077820ea67f
ridomin <ridomin@live.com>
2022-09-20 16:55:18 -0700

    works

--- webClient/index.js
+++ webClient/index.js
@@ -1,4 +1,3 @@
-import {Telemetry} from './mqttdevice.js'
 const mqttCreds = {
     hostName: 'localhost',
     port: 8080,
@@ -8,6 +7,12 @@ const mqttCreds = {
     password: 'password'
 }
 const start = () => {
+   let Telemetry
+   protobuf.load('mqttdevice.proto')
+    .then(function(root) {
+        Telemetry = root.lookupType('Telemetry')
+    })
+
     const appDiv = document.getElementById('app')
     const client = mqtt.connect(`${mqttCreds.useTls ? 'wss' : 'ws'}://${mqttCreds.hostName}:${mqttCreds.port}/mqtt`, {
                 clientId: mqttCreds.clientId, username: mqttCreds.userName, password: mqttCreds.password })
@@ -16,11 +21,11 @@ const start = () => {
                 })
                 
     client.on('message', (topic, message) => {
+        const tel = Telemetry.decode(message)
         const el = document.createElement('div')
-        el.innerText = topic + new String(message)
+        el.innerText = `${topic} ${tel.temperature}`
         appDiv.appendChild(el)
-        const t = Telemetry.toObject(message)
-        console.log(message)
+        console.log(tel)
     })
 }
 window.onload = start
