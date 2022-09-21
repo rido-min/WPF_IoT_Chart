54f32b134b82aa7905f9af12b395fa60e4e8831d
ridomin <ridomin@live.com>
2022-09-20 17:42:13 -0700

    push

--- webClient/index.js
+++ webClient/index.js
@@ -6,26 +6,50 @@ const mqttCreds = {
     userName: 'user',
     password: 'password'
 }
+
+const gbid = id => document.getElementById(id)
+
 const start = () => {
    let Telemetry
+   let Properties
    protobuf.load('mqttdevice.proto')
     .then(function(root) {
         Telemetry = root.lookupType('Telemetry')
+        Properties = root.lookupType('Properties')
     })
 
-    const appDiv = document.getElementById('app')
     const client = mqtt.connect(`${mqttCreds.useTls ? 'wss' : 'ws'}://${mqttCreds.hostName}:${mqttCreds.port}/mqtt`, {
                 clientId: mqttCreds.clientId, username: mqttCreds.userName, password: mqttCreds.password })
                 client.on('connect', () => {
                     client.subscribe('device/+/telemetry')
+                    client.subscribe('device/+/props/#')
                 })
                 
     client.on('message', (topic, message) => {
-        const tel = Telemetry.decode(message)
-        const el = document.createElement('div')
-        el.innerText = `${topic} ${tel.temperature}`
-        appDiv.appendChild(el)
-        console.log(tel)
+        const segments = topic.split('/')
+        const deviceId = segments[1]
+        if (deviceId) {
+            gbid('deviceId').innerText = deviceId
+        }
+        const what = segments[2]
+        if (what === 'telemetry')
+        {
+            const tel = Telemetry.decode(message)
+            const el = document.createElement('div')
+            el.innerText = `${topic} ${tel.temperature}`
+            gbid('app').appendChild(el)
+        }
+
+        if (what === 'props')
+        {
+            const prop = Properties.decode(message)
+            if (prop.sdkInfo) {
+                const el = document.createElement('div')
+                el.innerText = prop.sdkInfo
+                gbid('deviceProps').appendChild(el)
+            }
+        }
+        
     })
 }
 window.onload = start
